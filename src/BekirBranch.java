70d22c7116ae9ed6395243aabb0e8e480aa9a013
Bzyuksek <117347949+Bzyuksek@users.noreply.github.com>
2022-12-18 21:28:17 +0300

    Update BekirBranch.java

--- src/BekirBranch.java
+++ src/BekirBranch.java
@@ -2,5 +2,8 @@ public class BekirBranch {
 
     public static void main(String[] args) {
         System.out.println("Branch");
+         System.out.println("Branch1");
+         System.out.println("Branch2");
+         System.out.println("Branch3");
     }
 }
